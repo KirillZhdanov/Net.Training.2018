2a3bb98ffd97de9e36787ee151ca0d9b63574def
KirillZhdanov <jd_senar@mail.ru>
2018-03-21 13:23:18 +0200

    Update TestClass.cs

--- Day2/Newton/NUnit.Tests1/TestClass.cs
+++ Day2/Newton/NUnit.Tests1/TestClass.cs
@@ -7,19 +7,37 @@ using System.Threading.Tasks;
 
 namespace NUnit.Tests1
 {
+    /// <summary>
+    /// NUnit test for Newton's Method
+    /// </summary>
     [TestFixture]
-    public class TestClass
-    {
-        [TestCase(1, 5, 0.0001, ExpectedResult = 1)]
-        [TestCase(8, 3, 0.0001, ExpectedResult = 2)]
-        [TestCase(0.001, 3, 0.0001, ExpectedResult = 0.1)]
-        [TestCase(0.04100625, 4, 0.0001, ExpectedResult = 0.45)]
-        [TestCase(8, 3, 0.0001, ExpectedResult = 2)]
-        [TestCase(0.0279936, 7, 0.0001, ExpectedResult = 0.6)]
-        [TestCase(0.0081, 4, 0.1, ExpectedResult = 0.3)]
-        [TestCase(-0.008, 3, 0.1, ExpectedResult = -0.2)]
-        [TestCase(0.004241979, 9, 0.00000001, ExpectedResult = 0.545)]
-       public double Sqrt(double num, int degree, double eps)  => NewtonLogic.Newtons.FindNthRoot(num, degree, eps);
+    public class TestNewton
+    {/// <summary>
+     /// 
+     /// </summary>
+     /// <param name="num">Original number</param>
+     /// <param name="degree">Degree</param>
+     /// <param name="eps">It's your delta for approximation</param>
+     /// <param name="exp">Expected Result</param>
+        [TestCase(1, 5, 0.0001, 1)]
+        [TestCase(8, 3, 0.0001,  2)]
+        [TestCase(0.001, 3, 0.0001, 0.1)]
+        [TestCase(0.04100625, 4, 0.0001, 0.45)]
+        [TestCase(8, 3, 0.0001,  2)]
+        [TestCase(0.0279936, 7, 0.0001, 0.6)]
+        [TestCase(0.0081, 4, 0.1,  0.3)]
+        [TestCase(-0.008, 3, 0.1, -0.2)]
+        [TestCase(0.004241979, 9, 0.00000001,  0.545)]
        
+        public void Sqrt(double num, double degree, double eps,double exp)
+        {
+            double actual;
+            actual=NewtonLogic.Newtons.FindNthRoot(num, degree, eps);
+
+            Assert.AreEqual(exp, actual, 1);
+
+              
+        }
+
     }
 }
