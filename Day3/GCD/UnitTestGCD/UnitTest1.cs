7b33cd8a07b11416a73bbd85b20501e2fd28ab86
KirillZhdanov <jd_senar@mail.ru>
2018-03-22 19:04:22 +0200

    Add files via upload

---
+++ Day3/GCD/UnitTestGCD/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GCD;
+
+namespace GCD
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void EqualityOfMethods()
+        {
+            int stainsresult,euclideanresult;
+            euclideanresult = SearchingOfGCD.GetGcdByEuclideanAlgorithm(144, 20,40,36);
+            stainsresult = SearchingOfGCD.GetGcdByStainAlgorithm(144, 20,40,36);
+            Assert.AreEqual(stainsresult, euclideanresult);
+
+        }
+    }
+}
