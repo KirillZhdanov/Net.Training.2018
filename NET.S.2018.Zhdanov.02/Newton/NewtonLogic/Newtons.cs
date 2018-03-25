39845d3081a0499047e5d8512af5c16a19005f3e
KirillZhdanov <jd_senar@mail.ru>
2018-03-25 17:14:37 +0200

    Add files via upload

---
+++ NET.S.2018.Zhdanov.02/Newton/NewtonLogic/Newtons.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewtonLogic
+{
+    public class Newtons
+    {
+       public static double FindNthRoot(double num,double degree, double eps)
+        {
+            double x = num / degree;
+             if (num < 0)
+                throw new ArithmeticException("Number should be greater than 0!");
+            if (Math.Abs(eps) > 1)
+            throw new ArithmeticException("Epsilon should be less than 1");
+           double x1 = (1.0 / degree) * ((degree - 1.0) * x + num / Math.Pow((int)x, degree - 1));
+
+            while (Math.Abs(x1 - x) > eps)
+            {
+                x = x1;
+                x1 = (1.0 / degree) * ((degree - 1) * x + num / Math.Pow((int)x, degree - 1));
+            }
+            return x1;
+        }
+
+    }
+}
---
+++ NET.S.2018.Zhdanov.02/Newton/NewtonUnitTest/MSNewtonTest1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NewtonLogic;
+
+
+
+
+namespace NewtonLogic
+{
+    /// <summary>
+    /// Ms unit testing of Newton's Method
+    /// </summary>
+    [TestClass]
+    public class MSNewtonTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double test;
+           test = Newtons.FindNthRoot(8.0, 3.0,0.0001);
+           Assert.AreEqual(test,2.0,1);
+          
+        }
+        [TestMethod]
+        public void TestMethod()
+        {
+        
+            double test;
+            test = Newtons.FindNthRoot(1.0, 5.0, 0.0001);
+            
+            Assert.AreEqual(test, 1.0,1);
+            
+        }
+        [TestMethod]
+        public void Test()
+        {
+            double test,t;
+            test = Newtons.FindNthRoot(1.0, 5.0, 0.0001);
+            t = Math.Pow(1, 1 / 5);
+
+            test = Newtons.FindNthRoot(0.001, 3.0, 0.0001);
+            Assert.AreEqual(t, test,1);
+
+           
+        }
+        [TestMethod]
+        public void Test1()
+        {
+            double test;
+            test = Newtons.FindNthRoot(0.04100625, 4, 0.0001);
+          Assert.AreEqual(test, 0.45,1);
+        }
+        [TestMethod]
+        public void Test2()
+        {
+            double test;
+            
+            test = Newtons.FindNthRoot(8, 3, 0.0001);
+            Assert.AreEqual(test, 2.0,1);
+            
+        }
+        [TestMethod]
+        public void Test3()
+        {
+            double test;
+            test = Newtons.FindNthRoot(0.0279936, 7, 0.0001);
+            Assert.AreEqual(test, 0.6,1);
+           
+        }
+        [TestMethod]
+        public void Test4()
+        {
+            double test;
+            test = Newtons.FindNthRoot(0.0081, 4, 0.1);
+            Assert.AreEqual(test, 0.3,1);
+        }
+    }
+}
