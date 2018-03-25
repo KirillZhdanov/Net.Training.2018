763a06a4f55cf954fe19a6f1dd828866249fb842
KirillZhdanov <jd_senar@mail.ru>
2018-03-25 17:12:37 +0200

    Add files via upload

---
+++ NET.S.2018.Zhdanov.01/MergeSort/MergingTest/MergTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MergeLogic;
+
+namespace MergeLogic
+{
+    [TestClass]
+    public class MergTest
+    {
+        [TestMethod]
+        public void TestMerge()
+        {
+
+            // исходные данные
+
+            int[] expected = new int[5] { 1, 5, 6, 7, 8 };
+
+            int[] array = new int[5] { 5, 8, 7, 6, 1 };
+            int[] y = new int[5] { 1, 5, 6, 7, 8 };
+
+
+
+            // получение значения с помощью тестируемого метода
+            int []  actual=Merg.SortbyMergeMethod(array);
+
+            // сравнение ожидаемого результата с полученным
+            CollectionAssert.AreEqual(expected, actual);
+          
+
+
+
+
+        }
+    }
+}
---
+++ NET.S.2018.Zhdanov.01/QuickSort/QuickTest/SortTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuickSortLogic;
+
+namespace QuickSortLogic
+{
+    [TestClass]
+    public class SortTest
+    {
+        [TestMethod]
+        public void Testquick()
+        {
+            // исходные данные
+           
+            int[] expected =new int[5] {1,5,6,7,8 };
+           
+            int[] array = new int[5] { 5,8,7,6,1};
+
+            
+
+            
+            // получение значения с помощью тестируемого метода
+            Quicksort q = new Quicksort();
+            int []actual = q.Quicksrt(array, 0, array.Length - 1);
+            // сравнение ожидаемого результата с полученным
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
