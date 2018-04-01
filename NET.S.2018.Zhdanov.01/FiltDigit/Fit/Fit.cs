d07e4e38464ed376df502b28596010da55b07745
KirillZhdanov <jd_senar@mail.ru>
2018-04-01 20:47:31 +0200

    Add files via upload

---
+++ NET.S.2018.Zhdanov.01/FiltDigit/Fit/Fit.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiltDigit
+{
+    public class Fit
+    {
+        public static int [] Fitd(int [] arr, int digit)
+        {
+           
+            List<int> resDigit = new List<int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int numFromArr = arr[i];
+                if (FilterDigit(numFromArr, digit))
+                {
+                    resDigit.Add(arr[i]);
+                    numFromArr = 0;
+                }
+            }
+            return resDigit.ToArray();
+        }
+     
+        private static bool FilterDigit(int NumberFromArray, int Digit)
+        {
+            while (NumberFromArray != 0)
+            {
+                if (NumberFromArray % 10 == Digit || NumberFromArray == Digit)
+                {
+                    return true;
+                }
+                else
+                    NumberFromArray = NumberFromArray / 10;
+            }
+            return false;
+
+        }
+       
+    }
+}
