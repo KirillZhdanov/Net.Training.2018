c1fe64c26b7998b006403d0bb80dead9d121cb3c
KirillZhdanov <jd_senar@mail.ru>
2018-04-05 20:04:45 +0300

    Add files via upload

--- NET.S.2018.Zhdanov.07/Book/IFileBookListStorage.cs
+++ NET.S.2018.Zhdanov.07/Book/IFileBookListStorage.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Book
 {
-    interface IFileBookListStorage
+   public interface IFileBookListStorage<T>
     {
+        /// <summary>
+        /// To file
+        /// </summary>
+        /// <param name="writer"></param>
+        /// <param name="value"></param>
+        void SaveFileData(BinaryWriter writer, T value);
+        /// <summary>
+        /// Load data 
+        /// </summary>
+        /// <param name="reader"></param>
+        /// <returns></returns>
+        T LoadFileData(BinaryReader reader);
+
+
     }
 }
