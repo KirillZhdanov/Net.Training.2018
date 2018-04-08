1e33c4802b2141ef2277dab794e955f76933ece8
KirillZhdanov <jd_senar@mail.ru>
2018-04-08 16:55:39 +0300

    Add files via upload

---
+++ NET.S.2018.Zhdanov.08/GCD/GCD.Unit.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GCD;
+
+namespace GCD
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void EqualityOfMethods()
+        {
+            int stainsresult,euclideanresult;
+            euclideanresult = SearchingOfGCD.GcdByEuclideanAlgorithm(144, 20,40,36);
+            stainsresult = SearchingOfGCD.GcdByStainAlgorithm(144, 20,40,36);
+            Assert.AreEqual(stainsresult, euclideanresult);
+
+        }
+    }
+}
