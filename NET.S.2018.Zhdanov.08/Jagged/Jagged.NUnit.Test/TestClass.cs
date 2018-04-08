6cd06e5e6736f5d8933ec1d70b6bf69b43de83ef
KirillZhdanov <jd_senar@mail.ru>
2018-04-08 19:32:37 +0300

    Add files via upload

---
+++ NET.S.2018.Zhdanov.08/Jagged/Jagged.NUnit.Test/TestClass.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Linq;
+using Jagged;
+namespace NUnit.Tests1
+{
+    [TestFixture]
+    public class TestClass
+    {
+        
+
+        [Test]
+        public void Sum()
+        {
+            int[][] actual = new[] { new[] { 1 }, new[] { 0, 4, 7 },  new[] { 15, 21 } };
+            int[][] expected = new[] {  new[] { 1 }, new[] { 0,4, 7 }, new[] { 15, 21 } };
+            DelegateToInterfaceSort.Sort(actual, new ComparatorByMaxElement().Compare);
+           
+            CollectionAssert.AreEqual(actual, expected);
+        }
+    }
+}
