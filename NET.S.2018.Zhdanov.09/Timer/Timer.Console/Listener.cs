441edbeaf1359f7e943293ea4badb810721a3840
KirillZhdanov <jd_senar@mail.ru>
2018-04-12 17:59:21 +0300

    Add files via upload

---
+++ NET.S.2018.Zhdanov.09/Timer/Timer.Console/Listener.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Timer.Console.Program;
+
+
+namespace Timer.Console
+{
+    class Listener
+    {
+
+        
+
+        public int Number;
+
+        
+        /// <summary>
+        /// Listener
+        /// </summary>
+        /// <param name="number"></param>
+        public Listener(int number)
+        {
+            Number = number;
+        }
+
+        
+
+        #region Public Method
+
+        public void Timing(object sender, TimeLeft e)
+        {
+           ShowMessage($"Listener {Number}: Start timer\n\tSender: {sender}\n\tSeconds: {e.Seconds} timeleft.");
+        }
+
+        #endregion
+
+    }
+}
---
+++ NET.S.2018.Zhdanov.09/Timer/Timer.Console/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Timer.Clock;
+using static System.Console;
+using static System.Threading.Thread;
+
+namespace Timer.Console
+{
+   public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var timer = new Clock();
+            var listener1 = new Listener(1);
+            var listener2 = new Listener(2);
+            WriteLine("Add");
+            timer.Dispatcher += listener1.Timing;
+            timer.Dispatcher += listener2.Timing;
+            timer.RunTimer(5);
+            Sleep(10000);
+            WriteLine(" Remove first ");
+            timer.Dispatcher -= listener1.Timing;
+            timer.RunTimer(3);
+            Sleep(10000);
+            WriteLine(" Remove second ");
+            timer.Dispatcher -= listener2.Timing;
+            timer.RunTimer(3);
+            Sleep(10000);
+            ReadKey();
+        }
+
+        public static void ShowMessage(string str)
+        {
+            WriteLine(str);
+        }
+    }
+
+}
