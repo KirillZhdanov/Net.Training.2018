293d0fcc593b7b83c37e71e09d8cecac15d571b7
KirillZhdanov <jd_senar@mail.ru>
2018-04-12 17:54:00 +0300

    Add files via upload

---
+++ NET.S.2018.Zhdanov.09/Fibonacci/Fibonacci.Logic/FibGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    public  class FibGen
+    {    
+        /// <summary>
+        /// Generator of Fibonacci num's
+        /// </summary>
+        /// <param name="maxValue"></param>
+        /// <returns></returns>
+        public static int[] Generator(int maxValue)
+        {
+            int[] fibNumsArray=new int [2];
+            fibNumsArray[0] = 0;
+            fibNumsArray[1] = 1;
+            int i = 1;
+            while (true)
+            {
+                Array.Resize<int>(ref fibNumsArray, i+2);
+              fibNumsArray[i+1] = fibNumsArray[i - 1] + fibNumsArray[i];
+              i++;
+                if (fibNumsArray[i]>=maxValue)
+                    break;
+               
+            }
+            Array.Resize<int>(ref fibNumsArray, i);
+            return fibNumsArray;
+        }
+
+                
+       
+    }
+}
