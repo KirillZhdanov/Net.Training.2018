75a1e515d10918fdac9cbbfa7651026314559337
KirillZhdanov <jd_senar@mail.ru>
2018-04-12 17:54:41 +0300

    Add files via upload

---
+++ NET.S.2018.Zhdanov.09/Fibonacci/Fib.Test/TestClass.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fibonacci;
+
+namespace Fib.NUnit.Tests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+           int [] actual= Fibonacci.FibGen.Generator(18000);
+            int[] expected = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711 };
+
+
+
+
+
+            // TODO: Add your test code here
+            CollectionAssert.AreEqual(actual,expected );
+        }
+    }
+}
