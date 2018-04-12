14fc9d5dabe34bc578db292c589883f912edbdf7
KirillZhdanov <jd_senar@mail.ru>
2018-04-12 17:57:56 +0300

    Add files via upload

---
+++ NET.S.2018.Zhdanov.09/Timer/Timer.Logic/Clock.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Threading.Thread;
+
+
+namespace Timer
+{
+    public class Clock
+    {
+        #region Private Fields
+
+        private bool IsStarted;
+        private const int timeToSleep = 1000;
+
+        #endregion
+
+        #region Public Method
+
+        public async void RunTimer(int seconds)
+        {
+            if (IsStarted == true)
+                return;
+
+            await Task.Run(() =>
+            {
+                Sleep(seconds * timeToSleep);
+                OnTimeout(this, new TimeLeft(seconds));
+                IsStarted = false;
+            });
+        }
+
+        #endregion
+
+        #region Protected Method
+
+        protected virtual void OnTimeout(object sender, TimeLeft e)
+        {
+            if(Dispatcher!=null)
+            Dispatcher.Invoke(sender, e);
+        }
+
+        #endregion
+
+        
+
+        public event EventHandler<TimeLeft> Dispatcher;
+
+        
+
+
+    }
+}
---
+++ NET.S.2018.Zhdanov.09/Timer/Timer.Logic/TimeLeft.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timer
+{
+
+    public class TimeLeft:EventArgs
+    {
+
+        public int Seconds { get; private set; }
+
+        public TimeLeft(int seconds)
+        {
+            Seconds = seconds;
+        }
+    }
+}
