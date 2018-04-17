ab9781a8cefec097b7c3f594cffee3e1436252bd
KirillZhdanov <jd_senar@mail.ru>
2018-04-17 15:09:59 +0300

    Add files via upload

---
+++ NET.S.2018.Zhdanov.10/Fibonacci/Fibonacci.Logic/FibGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    public  class FibGen
+    {
+        /// <summary>
+        /// Generator of Fibonacci num's
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<long> Generator()
+        {
+            long previousElement = 0, currentElement = 1;
+
+            while (true)
+            {
+                
+                var nextElement = previousElement + currentElement;
+                yield return nextElement;
+                previousElement = currentElement;
+                currentElement = nextElement;
+            }
+        }
+
+                
+       
+    }
+}
