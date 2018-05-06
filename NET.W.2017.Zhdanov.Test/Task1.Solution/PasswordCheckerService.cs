11d0324de11e3e22c9db86127e5065fae9fe04e6
KirillZhdanov <jd_senar@mail.ru>
2018-05-06 22:07:26 +0300

    Add files via upload

---
+++ NET.W.2017.Zhdanov.Test/Task1.Solution/PasswordCheckerService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace Task1
+{
+    public class PasswordCheckerService
+    {
+        private IRepository repository ;
+
+        public IRepository Repository { get => repository; set => repository = value; }
+
+        public Tuple<bool, string> VerifyPassword(string password)
+        {
+            if (password == null)
+                throw new ArgumentException($"{password} is null arg");
+
+            if (password == string.Empty)
+                return Tuple.Create(false, $"{password} is empty ");
+
+            // check if length more than 7 chars 
+            if (password.Length <= 7)
+                return Tuple.Create(false, $"{password} length too short");
+
+            // check if length more than 10 chars for admins
+            if (password.Length >= 15)
+                return Tuple.Create(false, $"{password} length too long");
+
+            // check if password conatins at least one alphabetical character 
+            if (!password.Any(char.IsLetter))
+                return Tuple.Create(false, $"{password} hasn't alphanumerical chars");
+
+            // check if password conatins at least one digit character 
+            if (!password.Any(char.IsNumber))
+                return Tuple.Create(false, $"{password} hasn't digits");
+
+            Repository.Create(password);
+
+            return Tuple.Create(true, "Password is Ok. User was created");
+        }
+    }
+}
