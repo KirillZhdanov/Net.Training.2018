cddfb09075a5d2b37bec781239a521b553c7c62f
KirillZhdanov <jd_senar@mail.ru>
2018-05-06 22:44:21 +0300

    Add files via upload

--- NET.S.2018.Zhdanov.11/Matrix/Matrix.Logic/Adder.cs
+++ NET.S.2018.Zhdanov.11/Matrix/Matrix.Logic/Adder.cs
@@ -7,24 +7,64 @@ using System.Threading.Tasks;
 namespace Matrix
 {
 
-    public class Adder
+    public static class Adder
     {
 
-        private static SquareMatrix<T> GetSum<T>(SquareMatrix<T> matrix, SquareMatrix<T> other)
-          where T : IComparable<T>
+        #region Public Method
+
+        public static SquareMatrix<T> Sum<T>(this SquareMatrix<T> matrix, SquareMatrix<T> other)
+            where T : IComparable<T>
         {
-            var SumofMatrix = new T[matrix.Size][];
-            for (int i = 0; i < matrix.Size; i++)
-                SumofMatrix[i] = new T[matrix.Size];
+            Checker(matrix, other);
+            if (matrix.Size != other.Size)
+                throw new InvalidOperationException("Different sizes of matrixes.");
 
-            for (int i = 0; i < matrix.Size; i++)
-                    for (int j = 0; j < matrix.Size; j++)
-                        SumofMatrix[i][j] = (dynamic)matrix[i, j] + other[i, j];
-           
-            
-
-            return new SquareMatrix<T>(SumofMatrix);
+            return GetSum(matrix, other);
         }
+
+        #endregion
+
+        #region Private Methods
+
+        private static void Checker<T>(Matrix<T> matrix, Matrix<T> other)
+            where T : IComparable<T>
+        {
+            if (matrix == null)
+                throw new ArgumentNullException(nameof(matrix));
+            if (other == null)
+                throw new ArgumentNullException(nameof(other));
+        }
+
+        private static T[][] ClearArray<T>(SquareMatrix<T> matrix)
+            where T : IComparable<T>
+        {
+            var newMatrixValues = new T[matrix.Size][];
+            for (int i = 0; i < matrix.Size; i++)
+                newMatrixValues[i] = new T[matrix.Size];
+
+            return newMatrixValues;
+        }
+
+        private static SquareMatrix<T> GetSum<T>(SquareMatrix<T> matrix, SquareMatrix<T> other)
+            where T : IComparable<T>
+        {
+            var newMatrixValues = ClearArray(matrix);
+
+            try
+            {
+                for (int i = 0; i < matrix.Size; i++)
+                    for (int j = 0; j < matrix.Size; j++)
+                        newMatrixValues[i][j] = (dynamic)matrix[i, j] + other[i, j];
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return new SquareMatrix<T>(newMatrixValues);
+        }
+
+        #endregion
 
     }
 }
