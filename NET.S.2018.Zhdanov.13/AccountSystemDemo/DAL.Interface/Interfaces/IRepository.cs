d3f95bf8fe37a72d82db8db8bab6fcb54b64731f
KirillZhdanov <jd_senar@mail.ru>
2018-05-17 23:32:46 +0300

    Add files via upload

---
+++ NET.S.2018.Zhdanov.13/AccountSystemDemo/DAL.Interface/Interfaces/IRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using DAL.Interface.DTO;
+
+namespace DAL.Interface.Interfaces
+{
+    public interface IRepository
+    {
+        /// <summary>
+        /// ADD bank account
+        /// </summary>
+        /// <param name="bankAccount"></param>
+        void Add(BankAccountDto bankAccount);
+
+        /// <summary>
+        ///Update bank account 
+        /// </summary>
+        /// <param name="bankAccount"></param>
+        void Update(BankAccountDto bankAccount);
+
+        /// <summary>
+        /// Get bank accounts
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<BankAccountDto> GetBankAccounts();
+        /// <summary>
+        /// Get bank accounts according to their IBAN
+        /// </summary>
+        /// <param name="iban"></param>
+        /// <returns></returns>
+        BankAccountDto GetBankAccountByIban(string iban);
+
+    }
+}
